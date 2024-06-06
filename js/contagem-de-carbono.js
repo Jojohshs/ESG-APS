cae98903678388904360bb5ea6731a54e704a0a9
Jojohshs <133508501+Jojohshs@users.noreply.github.com>
2024-06-06 15:10:02 -0300

    Update contagem-de-carbono.js

--- js/contagem-de-carbono.js
+++ js/contagem-de-carbono.js
@@ -7,7 +7,7 @@ var num3 = parseFloat(document.getElementById ("numero3").value);
 var num3segundo = parseFloat(document.getElementById ("numero3segundo").value);
 var num4 = parseFloat(document.getElementById ("numero4").value);
 var resultadogas = num3 * num3segundo;
-var multi = num1 + num2 + num4;
+var multi = num1 * num2 * num4;
 var resultadototal = resultadogas + multi;
 document.getElementById("resultado").innerText = "Total:" +resultadototal;
 }
